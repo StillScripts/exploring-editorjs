52cc6248b7ab66bd97b3f28138b338f66ad2db43
StillScripts <80142739+StillScripts@users.noreply.github.com>
2023-02-25 21:21:28 +1100

    Separate React & Editor.js divs

--- apps/editor/src/main.tsx
+++ apps/editor/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("react") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
+
+const editor = document.querySelector<HTMLDivElement>("#editor") // get the app container <div> element
+
+// Update the innerHTML of the app container to contain the header, select input, and the sketch.
+if (editor) {
+  editor.innerHTML = `<h1>Editor.js</h1>`
+}
